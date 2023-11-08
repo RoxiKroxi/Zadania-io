41e7c074d6b51cf1da3ff585383a8d6582097d74
RoxiKroxi <148354034+RoxiKroxi@users.noreply.github.com>
2023-11-08 14:36:58 +0100

    Add files via upload

---
+++ io zad 4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+struct Produkt {
+    std::string nazwa;
+    float cena;
+};
+
+bool porownaj(const Produkt& produkt1, const Produkt& produkt2) {
+    return produkt1.cena > produkt2.cena;
+}
+
+int main() {
+    Produkt produkty[10];
+
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "Podaj nazwe produktu " << i + 1 << ": ";
+        std::cin >> produkty[i].nazwa;
+        std::cout << "Podaj cene produktu " << i + 1 << ": ";
+        std::cin >> produkty[i].cena;
+    }
+
+    std::sort(produkty, produkty + 10, porownaj);
+
+    std::cout << "Produkty od najdrozszych do najtanszych:\n";
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "Nazwa: " << produkty[i].nazwa << ", Cena: " << produkty[i].cena << std::endl;
+    }
+
+    return 0;
+}
+
